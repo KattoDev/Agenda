f79c315088f8a6849c2f716230ae4618369aac9c
Ritzz <154762212+RitzzRiver@users.noreply.github.com>
2024-07-13 16:30:34 -0500

    all requeriments done

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Implementaciones
 
-- [ ] El botón guardar no debe permitir añadir una persona a la agenda con una identificación
+- [X] El botón guardar no debe permitir añadir una persona a la agenda con una identificación
 que ya este registrada.
 - [X] Al guardar los datos deberá Limpiar todos los campos y ubicar el cursor en el campo
 identificación.
-- [ ] En botón [Ver datos de agenda] se deberá indicar con un mensaje si la agenda está vacía.
+- [X] En botón [Ver datos de agenda] se deberá indicar con un mensaje si la agenda está vacía.
 
 ## Extras
-- [ ] EXTRA Guardar datos en la app
+- [X] EXTRA Guardar datos en la app
--- src/Classes/JSONReader.java
+++ src/Classes/JSONReader.java
@@ -8,10 +8,9 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-
-
 
 public class JSONReader {
 
@@ -19,45 +18,41 @@ public class JSONReader {
     static final String key = "contacts";
     static Gson gson = new Gson();
 
-    public static void PrintContacts() {
+    public static List<Contact> GetContacts() {
+
+        List<Contact> contacts = new ArrayList<>();
+
         try {
             FileReader fr = new FileReader(filePath);
-            Type contactListType = new TypeToken<Map<String, List<Contact>>>() {}.getType();
+            Type contactListType = new TypeToken<Map<String, List<Contact>>>() {
+            }.getType();
 
             Map<String, List<Contact>> contactMap = gson.fromJson(fr, contactListType);
+            contacts = contactMap.get(key);
             try {
                 fr.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
 
-            List<Contact> contacts = contactMap.get(key);
-            for (Contact contact : contacts) {
-                System.out.println(contact);
-            }
-
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            System.err.println("No se ha encontrado el archivo " + e);
         }
+
+        return contacts;
     }
 
-    public static void SaveContacts() {
+    public static void CreateNewContact(String id, String name, String surname, String phoneNumber, String address) {
         try {
             // Read existing contacts
             FileReader fr = new FileReader(filePath);
-            Type contactListType = new TypeToken<Map<String, List<Contact>>>() {}.getType();
+            Type contactListType = new TypeToken<Map<String, List<Contact>>>() {
+            }.getType();
 
             Map<String, List<Contact>> contactMap = gson.fromJson(fr, contactListType);
             fr.close();
 
             List<Contact> contacts = contactMap.get(key);
-
-            // contact info
-            String id = "id3";
-            String name = "name3";
-            String surname = "surname3";
-            String phoneNumber = "PN3";
-            String address = "adress3";
 
             // create contact instance and add it to list
             Contact newContact = new Contact(id, name, surname, phoneNumber, address);
@@ -67,14 +62,21 @@ public class JSONReader {
             FileWriter fw = new FileWriter(filePath);
             gson.toJson(contactMap, fw);
             fw.close();
-
             System.out.println("Se creó el contacto");
         } catch (Exception e) {
+            System.err.println("Ha ocurrido un error guardando el contacto" + e);
         }
     }
 
-    public static void main(String[] args) {
-        SaveContacts();
-        PrintContacts();
+    public static void SaveContacts(List<Contact> contacts){
+        try {
+            Map<String, List<Contact>> contactMap = Map.of(key, contacts);
+            FileWriter fw = new FileWriter(filePath);
+            gson.toJson(contactMap, fw);
+            fw.close();
+
+        } catch (Exception e) {
+            System.err.println("No se ha podido guardar el contacto " + e);
+        }
     }
 }
